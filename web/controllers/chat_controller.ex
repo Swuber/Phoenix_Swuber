933bf9fe9b67d63cbff5c6fbd28772c3699b18fe
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-04-28 04:44:09 -0400

    3) channel chat; put core funct off homepage;

---
+++ web/controllers/chat_controller.ex
@@ -0,0 +1,7 @@
+defmodule UnfSwuber.ChatController do
+  use UnfSwuber.Web, :controller
+
+  def index(conn, _params) do
+    render conn, "index.html"
+  end
+end
---
+++ web/views/chat_view.ex
@@ -0,0 +1,3 @@
+defmodule UnfSwuber.ChatView do
+  use UnfSwuber.Web, :view
+end
