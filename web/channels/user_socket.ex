34cb597a16e18b5385514a793f0972ffbbf62a9c
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-04-28 04:29:28 -0400

    setup socket in channel room:lobby authorizing client access.

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule UnfSwuber.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", UnfSwuber.RoomChannel
+  channel "room:*", UnfSwuber.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
