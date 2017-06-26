529e948acb9f42e7bf25491cbc17d6834fb7595a
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-06-26 10:36:28 -0400

    gather values in web/channels/room with IO.inspect. :)

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -1,7 +1,7 @@
 defmodule UnfSwuber.RoomChannel do
   use Phoenix.Channel
   def join("room:lobby", _message, socket) do
-    IO.puts "room channel and not office channel"
+    side_effects("room:lobby", _message, socket)
     {:ok, socket}
   end
   def join("room:" <> _private_room_id, _params, _socket) do
@@ -10,5 +10,16 @@ defmodule UnfSwuber.RoomChannel do
   def handle_in("new_msg", %{"body" => body}, socket) do
     broadcast! socket, "new_msg", %{body: body}
     {:noreply, socket}
+  end
+  def side_effects("room:lobby", _message, socket) do
+    IO.puts ""
+    pid = self()
+    IO.puts "Manual Programmer gathering input follows."
+    checkalive = Process.alive?(self())
+    IO.inspect _message, label: " showing empty message:"
+    IO.inspect pid, label: " showing process ID matching pid of inspect:"
+    IO.inspect socket, label: "\n socket information when joining room:\n"
+    IO.puts "\nprocess alive? #{checkalive}"
+    IO.puts "room channel entered and not office channel"
   end
 end
