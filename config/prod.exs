4fe9b71899126bd6edff08a9dde02239abe61b7f
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-06-26 12:24:02 -0400

    2) configured to deploy to heroku;

---
+++ config/prod.exs
@@ -0,0 +1,18 @@
+use Mix.Config
+
+config :unf_swuber, UnfSwuber.Endpoint,
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "unf-swuber.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Do not print debug messages in production
+config :logger, level: :info
+
+# Configure your database
+config :unf_swuber, UnfSwuber.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
