53a3fa5c1e567cce95bcd6939afebee83ce60d13
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-08-10 08:15:52 -0400

    start the beginning of a chat test

---
+++ test/chats/chat_test.exs
@@ -0,0 +1,11 @@
+defmodule UnfSwuber.ChatTest do
+  use UnfSwuber.ChatCase
+
+  alias UnfSwuber # leaving the alias generic because will be going to multiple files.
+
+
+  test "client-side java socket message accepted in room rejected in office" do
+
+  end
+
+end
